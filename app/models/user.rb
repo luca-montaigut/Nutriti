e7a5698dac0636ff242553ff73f41f0eef088f47
thperchi <thibault.perchicot@outlook.fr>
2020-03-16 16:21:08 +0100

    oauth social media login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+		 :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
+def self.new_with_session(params, session)
+  super.tap do |user|
+    if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+      user.email = data["email"] if user.email.blank?
+    end
+  end
+end
+
+def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    user.first_name = auth.info.name   # assuming the user model has a name
+  end
+end
 end
