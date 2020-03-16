6541d3f86a8d2005c43b8c53fcf6feeff76a859a
Luca Montaigut <lmontaigut@gmail.com>
2020-03-16 16:38:58 +0100

    migration omniauthable

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,8 @@ ActiveRecord::Schema.define(version: 2020_03_16_141010) do
     t.string "last_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.integer "weight"
-    t.integer "height"
+    t.decimal "weight"
+    t.decimal "height"
     t.integer "age"
     t.string "gender"
     t.string "physical_activity"
