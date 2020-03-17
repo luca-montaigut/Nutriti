6e743ec27d36be0a8848d2ef30e2551712be9ecf
Luca Montaigut <lmontaigut@gmail.com>
2020-03-17 23:40:39 +0100

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'dashboard/index'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
   resources :user, only: [:show, :edit, :update]
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
   scope 'admin', module: 'admin', as: 'admin' do
     resources :dashboard, only: [:index]
