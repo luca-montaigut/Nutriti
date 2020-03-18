72cb977d0f618d8f9f34cd84dd8274b54d5255ff
thperchi <thibault.perchicot@outlook.fr>
2020-03-18 16:45:06 +0100

    fix social login

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,16 +19,16 @@ class User < ApplicationRecord
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+        self.email = data["email"] if user.email.blank?
       end
     end
   end
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.first_name = auth.info.name   # assuming the user model has a name
+	  self.email = auth.raw_info.email
+      self.password = Devise.friendly_token[0,20]
+      self.first_name = auth.raw_info.name
     end
   end
 
