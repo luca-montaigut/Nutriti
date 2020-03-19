da8d78d981af0ce211d5d27ca5046253071beb82
Luca Montaigut <lmontaigut@gmail.com>
2020-03-19 19:08:29 +0100

    better home page routing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,6 @@ class StaticPagesController < ApplicationController
     if user_signed_in?
       @users_week = Week.find_by(user_id: current_user.id)
       redirect_to week_path(@users_week.id)
-    else
-      redirect_to landing_path
     end
   end
 
