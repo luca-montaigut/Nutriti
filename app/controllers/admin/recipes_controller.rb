2cd16a211f19ddbdeb7bc157b9b1314e43ac436b
Luca Montaigut <lmontaigut@gmail.com>
2020-03-20 10:07:02 +0100

    some display in admin dashboard

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,34 +1,26 @@
 class Admin::RecipesController < Admin::ApplicationController
   before_action :set_admin_recipe, only: [:show, :edit, :update, :destroy]
 
-  # GET /admin/recipes
-  # GET /admin/recipes.json
   def index
     @admin_recipes = Recipe.all
-
-	respond_to do |format|
-	  format.html
-	  format.csv { send_data @admin_recipes.recipes_to_csv }
-	end
+    @categories = ["Starter", "Dish", "Dessert", "Drink", "Complement"]
+    respond_to do |format|
+      format.html
+      format.csv { send_data @admin_recipes.recipes_to_csv }
+    end
   end
 
-  # GET /admin/recipes/1
-  # GET /admin/recipes/1.json
   def show
   end
 
-  # GET /admin/recipes/new
   def new
     @admin_recipe = Recipe.new
     @admin_recipe.join_recipe_foods.build
   end
 
-  # GET /admin/recipes/1/edit
   def edit
   end
 
-  # POST /admin/recipes
-  # POST /admin/recipes.json
   def create
     @admin_recipe = Recipe.new(admin_recipe_params)
 
@@ -41,8 +33,6 @@ class Admin::RecipesController < Admin::ApplicationController
     end
   end
 
-  # PATCH/PUT /admin/recipes/1
-  # PATCH/PUT /admin/recipes/1.json
   def update
     @admin_recipe.join_recipe_foods.destroy_all
     respond_to do |format|
@@ -54,8 +44,6 @@ class Admin::RecipesController < Admin::ApplicationController
     end
   end
 
-  # DELETE /admin/recipes/1
-  # DELETE /admin/recipes/1.json
   def destroy
     @admin_recipe.join_recipe_foods.destroy_all
     @admin_recipe.destroy
@@ -65,12 +53,10 @@ class Admin::RecipesController < Admin::ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_admin_recipe
       @admin_recipe = Recipe.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def admin_recipe_params
       params
       .require(:recipe)
