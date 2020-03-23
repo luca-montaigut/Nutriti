262d26594d0aaae000b47415427934361ffb1903
thperchi <thibault.perchicot@outlook.fr>
2020-03-23 16:12:38 +0100

    delete omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#home"
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users
   
   scope 'users', module: 'users' do
     resources :users, only: [:show, :edit, :update]
