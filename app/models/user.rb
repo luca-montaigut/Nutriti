4061220430664c3a56fe6bf50534fa2dd6b26024
BaptisteIg <baptiste4498@gmail.com>
2020-03-26 11:28:58 +0100

    button_menu + take_off_name_at_registration + user_profile_front

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,6 @@ class User < ApplicationRecord
     presence: true,
     uniqueness: true,
     format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
 
   has_one :week
   has_one :breakfast
